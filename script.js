943420c7d69d3ca29295458f4702132e9f4f10a9
N0Arxan <arshan.hz@yahoo.com>
2025-01-17 19:17:41 +0100

    Seguimiento

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let start = document.getElementById("start");
+const start = document.getElementById("start");
 const colors = ['red','green','blue'];
 const green = document.getElementById('green');
 const blue = document.getElementById('blue');
 const red = document.getElementById('red');
-let level = 1;
-let playerNumMove = 0;
-let playerMove = [];
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms)); // Fixed sleep function
+const score = document.getElementById("score");
+const msg = document.getElementsByTagName("h2")[0];
+const compareArrays = (a, b) => {
+    return JSON.stringify(a) === JSON.stringify(b);
+};
+const sleep = (ms)=> {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function generateMachineMove(num){
+
+let level = 2;
+let playerMove =[];
+
+
+
+function generateMachineMove(level){
     let machineMove = [];
-    for (let i = 0;i < num;i++){
+    for (let i = 0;i < level;i++){
         let randomElem = colors[Math.floor(Math.random() * colors.length)];
         machineMove.push(randomElem);
     }
@@ -21,6 +28,7 @@ function generateMachineMove(num){
 }
 
 async function displayMachineMove(array = ["red", "blue", "green"]) {
+    msg.innerHTML = "Look carefully ... ";
     for (const color of array) {
         if (color === "red") {
             red.classList.add("active-red");
@@ -43,22 +51,57 @@ async function displayMachineMove(array = ["red", "blue", "green"]) {
         await sleep(1700)
     }
 }
-function addEL(){
+function addEL(machineMove,playerMove){
+    msg.innerHTML = " Your turn ";
     red.addEventListener("click", red.handler = () => {
         playerMove.push(red.getAttribute("value"));
-        playerNumMove++
+        if (playerMove.length === level){
+            check(machineMove,playerMove);
+        }
     })
     blue.addEventListener("click",blue.handler = () => {
         playerMove.push(blue.getAttribute("value"));
-        playerNumMove++
+        if (playerMove.length === level){
+            check(machineMove,playerMove);
+        }
     })
     green.addEventListener("click",green.handler = () => {
         playerMove.push(green.getAttribute("value"));
-        playerNumMove++
+        if (playerMove.length === level){
+            check(machineMove,playerMove);
+        }
     })
 }
 function removeEL(){
     red.removeEventListener("click", red.handler );
     blue.removeEventListener("click", blue.handler );
     green.removeEventListener("click", green.handler );
+}
+
+function check(playerMove,machineMove){
+    removeEL();
+    msg.innerHTML = "Simon Says";
+    console.log(playerMove,machineMove)
+    if(compareArrays(playerMove,machineMove)){
+        console.log("you won");
+        level++;
+        let int = parseInt(score.innerHTML);
+        int += 1;
+        score.innerHTML = int;
+        playerMove =[];
+        main(level);
+    }else{
+        console.log("you lose");
+        score.innerHTML = 0;
+        level = 2 ;
+        playerMove =[];
+    }
+}
+
+async function main(level){
+    
+    let machineMove = generateMachineMove(level);
+    displayMachineMove(machineMove);
+    await new Promise(resolve => setTimeout(resolve, level*2700));
+    addEL(machineMove,playerMove);
 }
